e5e42a7d095febaf9b2d64e22a7d02c600678320
Aron Boyette <aron.boyette@xanotech.com>
2015-10-27 15:40:18 -0400

    Updated version from 1.5 to 1.6.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("XTools: common tools")]
 [assembly: AssemblyCompany("Xanotech LLC")]
-[assembly: AssemblyCopyright("© 2014 Xanotech LLC")]
+[assembly: AssemblyCopyright("© 2015 Xanotech LLC")]
 [assembly: AssemblyProduct("XTools")]
 
 [assembly: AssemblyFileVersion("1.6")]
